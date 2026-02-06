63471445ebdf9540a766bcb170a75f4e47d4163b
Jun <icloudcualong@gmail.com>
2026-02-06 10:29:44 +0700

    tạo repo

---
+++ src/main/java/mera/getdatatodb/GetdataToDbApplication.java
@@ -0,0 +1,13 @@
+package mera.getdatatodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetdataToDbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GetdataToDbApplication.class, args);
+  }
+
+}
---
+++ src/test/java/mera/getdatatodb/GetdataToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package mera.getdatatodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetdataToDbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
